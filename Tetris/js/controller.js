b33c3b60cd00ac2918a20d9eee23d5189e178854
Boof <97455552+hexahigh@users.noreply.github.com>
2022-10-21 11:13:58 +0200

    added wasd controls to tetris

--- Tetris/js/controller.js
+++ Tetris/js/controller.js
@@ -6,6 +6,12 @@ document.body.onkeydown = function( e ) {
         38: 'rotate',
         32: 'drop'
     };
+    var keys = {
+        68: 'left',
+        65: 'right',
+        83: 'down',
+        87: 'rotate',
+    };
     if ( typeof keys[ e.keyCode ] != 'undefined' ) {
         keyPress( keys[ e.keyCode ] );
         render();
