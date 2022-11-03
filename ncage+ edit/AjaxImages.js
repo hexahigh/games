ea6545949f2edbfe294aabdeb191d9bee1372369
Boof <97455552+hexahigh@users.noreply.github.com>
2022-11-03 10:45:40 +0100

    added 4 images of cat

---
+++ ncage+ edit/AjaxImages.js
@@ -0,0 +1,6 @@
+var AjaxImages = [
+    "https://i.imgur.com/1FVuDxZ.jpg",
+    "https://i.imgur.com/w5HP7jI.jpg",
+    "https://i.imgur.com/s4YQgOT.jpg",
+    "https://i.imgur.com/mWNk9BD.jpg"    
+];
