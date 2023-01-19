a6b80947e03454cdd3096e23ba4e1b14c79d603d
Boof <97455552+hexahigh@users.noreply.github.com>
2023-01-19 08:54:45 +0100

    fix

--- tools/bandwidthuser.sh
+++ tools/bandwidthuser.sh
@@ -1,16 +1,9 @@
 #Settings
 echo Please input the url of the item you wish to download.
 read link
-echo Do you want to display how many times you have downloaded the file?
-echo True/False
-read shownum
-let num = 0
 #The downloader
 for (( ; ; ))
 do
     wget $link -O DELETEME
     rm -rf DELETEME
-    let num ++ 1
-    while [ "$shownum" = "true" ]
-        echo $num
 done
