7681c280dd752f30b2e7337244b6ea36ac600be1
Boof <97455552+hexahigh@users.noreply.github.com>
2023-03-15 10:44:38 +0100

    update

--- tools/XylkJweJ/scripts/secret.js
+++ tools/XylkJweJ/scripts/secret.js
@@ -26,10 +26,7 @@ async function checkSecret() {
     while (doCheck == true) {
         await delay(50)
         if (sixPressed == true && ninePressed == true) {
-            //document.getElementById("bodyId").innerHTML = "whatever";
-            document.write(<div style="position:absolute; left: 0; right: 0; bottom: 0; top: 0px;">
-                <iframe sandbox="allow-scripts allow-popups" width="100%" height="100%" frameborder="0" src="https://herremann.edu.eu.org"></iframe>
-            </div>)
+            document.getElementById("wrapper").style.display = "none";
         }
     }
 }
