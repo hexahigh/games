ca9468b36756b9c89d7097778590bb3924a60a1e
Boof <97455552+hexahigh@users.noreply.github.com>
2023-03-27 11:36:29 +0200

    update

--- Games/move-cube/js/draw.js
+++ Games/move-cube/js/draw.js
@@ -1,13 +1,13 @@
 function setup() {
-    createCanvas(40, 40);
+    createCanvas(400, 400);
 }
 
 function draw() {
     background(220);
     //For (var BEGIN; END; INTERVAL){
     //DO SOMETHING }
-    for (var x = 0; x < width; x += width / 5) {
-        for (var y = 0; y < height; y += height / 10) {
+    for (var x = 0; x < width; x += width / 40) {
+        for (var y = 0; y < height; y += height / 40) {
             stroke(0);
             strokeWeight(1);
             line(x, 0, x, height);
