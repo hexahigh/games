b95c122693e5816d723942b3113a90e9fe5c04bc
Boof <97455552+hexahigh@users.noreply.github.com>
2023-04-28 10:24:18 +0200

    a

--- other/urls to array/index.js
+++ other/urls to array/index.js
@@ -1,11 +1,19 @@
 const textInput = document.getElementById("textInput");
 const processButton = document.getElementById("processButton");
 
+var inputText = "NaN"
+var lines = "NaN"
+var modifiedArr = "NaN"
+
 processButton.addEventListener("click", function () {
-    const inputText = textInput.value;
-    const lines = inputText.split(/[\n\r]+/);
+    inputText = textInput.value;
+    lines = inputText.split(/[\n\r]+/);
     console.log(lines);
-    const arr = ['first', 'second', 'third'];
-    const modifiedArr = arr.map(i => '"' + i + '",');
+    modifiedArr = linus.map(i => '"' + i + '",');
     console.log(modifiedArr)
 });
+
+function addArrayToTextarea() {
+    const textarea = document.getElementById("myTextarea");
+    textarea.value = modifiedArr
+  }
