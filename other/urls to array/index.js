694fe48e7f4a62fa03ced72690adfc2296cacb21
Boof <97455552+hexahigh@users.noreply.github.com>
2023-04-28 10:19:00 +0200

    e

--- other/urls to array/index.js
+++ other/urls to array/index.js
@@ -2,7 +2,10 @@ const textInput = document.getElementById("textInput");
 const processButton = document.getElementById("processButton");
 
 processButton.addEventListener("click", function () {
-  const inputText = textInput.value;
-  const lines = inputText.split(/[\n\r]+/);
-  console.log(lines);
+    const inputText = textInput.value;
+    const lines = inputText.split(/[\n\r]+/);
+    console.log(lines);
+    const arr = ['first', 'second', 'third'];
+    const modifiedArr = arr.map(i => '"' + i + '",');
+    console.log(modifiedArr)
 });
