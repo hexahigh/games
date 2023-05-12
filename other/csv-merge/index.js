14f44a929bcc9384b0245488ea41131caffef432
Boof <97455552+hexahigh@users.noreply.github.com>
2023-05-12 11:33:22 +0200

    fix

--- other/csv-merge/index.js
+++ other/csv-merge/index.js
@@ -4,7 +4,7 @@ var array256 = "null"
 var array1 = "null"
 var output = "null"
 var linesDone = "1"
-const outputArea = document.getElementById("outputArea").value
+const outputArea = document.getElementById("outputArea")
 
 function loadFiles() {
     loadString()
@@ -62,7 +62,7 @@ function mergeToCsv() {
             Astring = arrayString[linesDone];
             A1 = array1[linesDone];
             output = Astring + "," + Amd5 + "," + A1 + "," + A256
-            outputArea = outputArea + "\n" + output
+            outputArea.value = outputArea.value + "\n" + output
         }
         linesDone + 1
     }
