43413fa27391436b3c6a2a77c857648046a2f31c
hexahigh <97455552+hexahigh@users.noreply.github.com>
2023-05-13 12:52:32 +0200

    update

--- other/csv-merge/index.js
+++ other/csv-merge/index.js
@@ -3,7 +3,7 @@ var arrayMD5 = "null"
 var array256 = "null"
 var array1 = "null"
 var output = "null"
-var linesDone = 1
+var linesDone = 10000
 const outputArea = document.getElementById("outputArea")
 
 function loadFiles() {
@@ -57,7 +57,7 @@ function mergeToCsv() {
     console.log("Lenght of array is " + arrayString.length)
     for (x in arrayString) {
         let length = arrayString.length
-        if(linesDone < 10000) {
+        if(linesDone < 30000) {
             Amd5 = arrayMD5[linesDone];
             Asha1 = array256[linesDone];
             Astring = arrayString[linesDone];
